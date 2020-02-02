3cfe4e9c7b39d28a095ccb5691aa24051cf211bf
Youssless <yaseenlahmami98@gmail.com>
2020-02-02 11:15:00 +0000

    working beta

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -2,19 +2,59 @@ import * as THREE from './threejs/build/three.module.js';
 
 import { OrbitControls } from './threejs/examples/jsm/controls/OrbitControls.js';
 
-const RADIUS = 7;
+const RADIUS = 10;
 const WIDTH_SEGMENTS = 100;
 const HEIGHT_SEGMENTS = 100;
-const lon_change = 28;
+const lon_change = 32;
+const lat_change = 2;
 const container = document.getElementById('worldcanvas')
+const num_affected_countries = 30;
+const nrotate = 10;
 
 var camera, scene, renderer, controls;
-let points = [];
+var points = [];
+
+var url = 'https://api.virusvis.dumitruvulpe.com';
+
+function getCountryCardinal(country) {
+    return new Promise((resolve, reject) => {
+        fetch(`${url}/coords?country=${country}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((row) => {
+                console.log(row);
+                resolve(row);
+            }, (err) => {
+                console.log(err);
+                reject(err);
+            });
+    });
+}
+
+function getAffectedCountries() {
+    return new Promise((resolve, reject) => {
+        fetch(`${url}/affected_countreis`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((row) => {
+                console.log(row);
+                resolve(row);
+            }, (err) => {
+                console.log(err);
+                reject(err);
+            });
+    });
+}
+
+
+
 
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({ antialias: true });
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement)
@@ -26,18 +66,15 @@ function load_world() {
     var group = new THREE.Group();
     var points = [];
     var texture = new THREE.TextureLoader().load('map.png');
+    // texture.rotation = 0.01;//nrotate * 180/3.14;
     var geometry = new THREE.SphereGeometry(RADIUS, WIDTH_SEGMENTS, HEIGHT_SEGMENTS);
     var material = new THREE.MeshBasicMaterial({ map: texture });
     var sphere = new THREE.Mesh(geometry, material);
     //controls = new OrbitControls(camera, renderer.domElement);
 
-    for(var i = 0; i < sphere.geometry.vertices.length; i++) {
-        console.log(sphere.geometry.vertices[i]);
-    }
-    
     var point_geometry = new THREE.SphereGeometry(0.07, 50, 50);
-    var point_material = new THREE.MeshBasicMaterial({color: 0xffffff});
-    
+    var point_material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+
     // for(var i = 0; i < sphere.geometry.vertices.length; i++) {
     //     var point = new THREE.Mesh(point_geometry, point_material);
     //     point.position.set(sphere.geometry.vertices[i].x, sphere.geometry.vertices[i].y, sphere.geometry.vertices[i].z);
@@ -48,12 +85,36 @@ function load_world() {
     //     group.add(points[i]);
     // }
 
-    var point = new THREE.Mesh(point_geometry, point_material);
-    placeObjectOnPlanet(point, 36.5042, 99.5919-lon_change, RADIUS);
-    group.add(sphere);
-    group.add(point);
-    scene.add(group);
-    controls = new OrbitControls(camera, renderer.domElement);
+    getAffectedCountries()
+        .then((result) => {
+            result.forEach((e) => {
+                getCountryCardinal(e.CName)
+                    .then((res) => {
+
+                        for (var i = 0; i < num_affected_countries; i++) {
+                            var point = new THREE.Mesh(point_geometry, point_material);
+                            placeObjectOnPlanet(point, res[0].Latitude + lat_change, res[0].Longitude - lon_change, RADIUS);
+                            console.log(e)
+                            //points.geometry.boundingSphere.radius = 
+                            points.push(point);
+                            // console.log(point);
+                        }
+
+                        group.add(sphere);
+                        for (var i = 0; i < points.length; i++) {
+                            group.add(points[i]);
+                        }
+                        group.add(point);
+
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+            })
+            scene.add(group);
+            controls = new OrbitControls(camera, renderer.domElement);
+            
+        });
+
 }
 
 function placeObjectOnPlanet(object, lat, lon, radius) {
@@ -72,9 +133,9 @@ function load_points() {
     var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     var point = new THREE.Mesh(geometry, material);
 
-   
+
     scene.add(point);
-    
+
 }
 
 var animate = function () {
