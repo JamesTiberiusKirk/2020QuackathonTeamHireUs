264e7c19b96ca78ca267173bb2aed38aec866bae
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-02 01:23:13 +0000

    Fixed certbot

--- certbot.sh
+++ certbot.sh
@@ -1,8 +1,8 @@
 docker run -it --rm \
--v /root/team5agile/Software_Code/docker-volumes/cert_bot/certs/:/etc/letsencrypt \
--v /root/team5agile/Software_Code/docker-volumes/cert_bot/certs_data/:/var/lib/letsencrypt \
--v "/root/team5agile/Software_Code/docker-volumes/cert_bot/certbot_logs/:/var/log/letsencrypt" \
--v /root/team5agile/Software_Code/docker-volumes/cert_bot/certbot/root/:/root/ \
+-v /root/2020QuackathonTeamHireUs/docker-volumes/cert_bot/certs/:/etc/letsencrypt \
+-v /root/2020QuackathonTeamHireUs/docker-volumes/cert_bot/certs_data/:/var/lib/letsencrypt \
+-v "/root/2020QuackathonTeamHireUs/docker-volumes/cert_bot/certbot_logs/:/var/log/letsencrypt" \
+-v /root/2020QuackathonTeamHireUs/docker-volumes/cert_bot/certbot/root/:/root/ \
 certbot/certbot \
 certonly --manual --preferred-challenges dns \
 --server https://acme-v02.api.letsencrypt.org/directory \
