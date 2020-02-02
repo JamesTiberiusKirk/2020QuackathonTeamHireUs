f4f7d553eb066330ec307a9e9e5bc3792b8be840
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-02 02:25:50 +0000

    Refactored db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,38 @@
 const mysql = require('mysql');
-const creds = require('../config/config').db_creds;
+const creds = require('../config/config').db_creds
 
-var conn = mysql.createConnection(creds);
+class Db {
+    constructor() {
+        this.conn;
+        this.initConnection();
+        this.handleConnection();
+        // this.handleError();
+    }
 
-conn.connect((err)=>{
-    if (err) throw err;
-    console.log('Database connection made...');
-});
+    initConnection() {
+        this.conn = mysql.createConnection(creds);
+    }
 
+    handleConnection(){
+        this.conn.connect((err) => {
+            if (err) {
+                console.error('MySql error connecting: ' + err.stack);
+                return;
+            }
 
-module.exports=conn;
+            console.log('MySql connected...');
+        });
+    }
+
+//     handleError(){
+//         this.conn.on('error',(err)=>{
+//             if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//                 this.initConnection();
+//             } else {
+//                 throw err;
+//             }
+//         });
+//     }
+}
+
+module.exports = { Db };
