44d5b49146f5be72ad39d59e2d31dabecdbc787b
Youssless <yaseenlahmami98@gmail.com>
2020-02-02 04:04:11 +0000

    world now moves with mouse drag

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,27 +1,37 @@
+import * as THREE from './threejs/build/three.module.js';
+
+import { OrbitControls } from './threejs/examples/jsm/controls/OrbitControls.js';
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 var renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement)
 
-const RADIUS = 5;
-const WIDTH_SEGMENTS = 20;
-const HEIGHT_SEGMENTS = 20;
+const RADIUS = 7;
+const WIDTH_SEGMENTS = 1000;
+const HEIGHT_SEGMENTS = 1000;
 
-var texture = new THREE.TextureLoader().load('land_ocean_ice_cloud_2048.jpg');
+var texture = new THREE.TextureLoader().load('map.png');
 
 var geometry = new THREE.SphereGeometry(RADIUS, WIDTH_SEGMENTS, HEIGHT_SEGMENTS);
-var material = new THREE.MeshBasicMaterial({map: texture});
+var material = new THREE.MeshBasicMaterial({ map: texture });
 var sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 camera.position.z = 10;
+var controls = new OrbitControls(camera, renderer.domElement);
 
-var animate = function() {
+var animate = function () {
     requestAnimationFrame(animate);
-    sphere.rotation.x += 0.2;
-    sphere.rotation.y += 0.2;
+    // sphere.rotation.x += 0.001;
+    // sphere.rotation.y += 0.001;
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+var render = function () {
     renderer.render(scene, camera);
 }
 
